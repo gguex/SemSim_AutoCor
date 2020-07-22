9e513a869e6df0196e15484dd20bbfeceea77073
Mattia Egloff <mattia.egloff@unil.ch>
2020-07-22 13:23:50 +0200

    create tiles with other pos than names also

--- code_python/text_preprocessing.py
+++ code_python/text_preprocessing.py
@@ -2,18 +2,21 @@ import nltk
 from flair.models import SequenceTagger
 from flair.data import Sentence
 import re
+from tqdm import tqdm
 
 # --- Parameters --- #
 
-base_path = "/home/gguex/Documents"
+base_path = "/home/megloff1/Desktop/ARTICLE/SemSim_AutoCor/"
 
 # Path of the raw text file
-text_file_path = base_path + "/data/corpora/pg43936-The_Wonderful_Wizard_of_Oz.txt"
+text_file_path = base_path + "corpora/pg43936-The_Wonderful_Wizard_of_Oz.txt"
 # Path of the outputted text file with two columns : token, POS tag
-tagged_output_path =  base_path + "/data/corpora/The_Wonderful_Wizard_of_Oz_tagged.txt"
+tagged_output_path =  base_path + "corpora/The_Wonderful_Wizard_of_Oz_tagged.txt"
 # Path of the outputted text file with only nouns
-noun_only_output_path =  base_path + "/data/corpora/The_Wonderful_Wizard_of_Oz_noun_only.txt"
-verbs_only_output_path = base_path + "/data/corpora/The_Wonderful_Wizard_of_Oz_verbs_only.txt"
+noun_only_output_path =  base_path + "corpora/The_Wonderful_Wizard_of_Oz_noun_only.txt"
+verb_only_output_path = base_path + "corpora/The_Wonderful_Wizard_of_Oz_verb_only.txt"
+adjective_only_output_path = base_path + "corpora/The_Wonderful_Wizard_of_Oz_adjective_only.txt"
+adverb_only_output_path = base_path + "corpora/The_Wonderful_Wizard_of_Oz_adverb_only.txt"
 
 # --- POS tagging of the file --- #
 
@@ -27,7 +30,7 @@ sentence_list = nltk.sent_tokenize(text_string)
 # Using flair POS tagging on each sentence
 tagger = SequenceTagger.load("pos")
 tagged_sentence_list = []
-for sentence in sentence_list:
+for sentence in tqdm(sentence_list):
     sent = Sentence(sentence.replace("\n", " "))
     tagger.predict(sent)
     tagged_sentence_list.append(sent)
@@ -37,34 +40,41 @@ for sentence in sentence_list:
 # Saving the file with all tokens and tags
 with open(tagged_output_path, "w") as output_tagged_file:
     output_tagged_file.write("token\ttag\n")
-    for tagged_sentence in tagged_sentence_list:
+    for tagged_sentence in tqdm(tagged_sentence_list):
         for token in tagged_sentence:
             output_tagged_file.write(token.text + "\t" + token.get_tag("pos").value + "\n")
 
-# Saving the file with only nouns
-allowed_tag = ["NN", "NNS", "NNP", "NNPS"]
+
+# Saving the file with one type of part of speach
+#
+# NN <- nouns
+# VB <- verbs
+# JJ <- adjectives
+# RB <- adverbs
+
 lemmatizer = nltk.stem.WordNetLemmatizer()
-with open(noun_only_output_path, "w") as noun_only_file:
-    for tagged_sentence in tagged_sentence_list:
+with open(noun_only_output_path, "w") as noun_only_file, \
+    open(verb_only_output_path, "w") as verb_only_file, \
+    open(adjective_only_output_path, "w") as adjective_only_file, \
+    open(adverb_only_output_path, "w") as adverb_only_file:
+    for tagged_sentence in tqdm(tagged_sentence_list):
         for token in tagged_sentence:
-            if token.get_tag("pos").value in allowed_tag:
-                processed_token = token.text.lower()
-                processed_token = re.sub(r'[^\w\s\-]', "", processed_token)
-                processed_token = re.sub(r'\-', " ", processed_token)
-                processed_token = lemmatizer.lemmatize(processed_token)
+            pos = token.get_tag("pos").value[:2]
+            if pos not in ("NN", "VB", "JJ", "RB"):
+              continue;
+            processed_token = token.text.lower()
+            processed_token = re.sub(r'[^\w\s\-]', "", processed_token)
+            processed_token = re.sub(r'\-', " ", processed_token)
+            processed_token = lemmatizer.lemmatize(processed_token)
+            if pos == "NN":
                 noun_only_file.write(processed_token + " ")
+            elif pos == "VB":
+                verb_only_file.write(processed_token + " ")
+            elif pos == "JJ":
+                adjective_only_file.write(processed_token + " ")
+            else:
+                adverb_only_file.write(processed_token + " ")
         noun_only_file.write("\n")
-
-
-# Saving the file with only verbs
-lemmatizer = nltk.stem.WordNetLemmatizer()
-with open(verbs_only_output_path, "w") as verbs_only_file:
-    for tagged_sentence.startswith("VB"):
-        for token in tagged_sentence:
-            if token.get_tag("pos").value in allowed_tag:
-                processed_token = token.text.lower()
-                processed_token = re.sub(r'[^\w\s\-]', "", processed_token)
-                processed_token = re.sub(r'\-', " ", processed_token)
-                processed_token = lemmatizer.lemmatize(processed_token)
-                verbs_only_file.write(processed_token + " ")
-        verbs_only_file.write("\n")
+        verb_only_file.write("\n")
+        adjective_only_file.write("\n")
+        adverb_only_file.write("\n")
