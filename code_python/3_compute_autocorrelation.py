25924deef72f5117c8d30841e307a7983875c060
Mattia Egloff <mattia.egloff@unil.ch>
2020-11-03 10:58:48 +0100

    created functions to compute in one go

--- code_python/3_compute_autocorrelation.py
+++ code_python/3_compute_autocorrelation.py
@@ -8,10 +8,8 @@ from tqdm import tqdm
 # --- Parameters --- #
 
 # Path of the text file with only nouns, verbs, adjectives or adverbs to compute autocorrelation
-input_file = "Animal_farm_nouns.txt"
-
-# Name of the tag for the similarity
-sim_tag = "wesim"
+input_files = ("The_WW_of_Oz_nouns.txt", "The_WW_of_Oz_verbs.txt", "Animal_farm_nouns.txt", "Animal_farm_verbs.txt")
+sim_tags = ("resnik", "wu-palmer", "leacock-chodorow", "wesim")
 
 # Autocorrelation range limit
 max_range = 50
@@ -20,83 +18,90 @@ max_range = 50
 
 # Getting the base path (must run the script from a folder inside the "SemSim_Autocor" folder)
 working_path = os.getcwd()
-base_path = str.split(working_path, "SemSim_AutoCor")[0] + "SemSim_AutoCor/"
 
-# Path of the text file
-file_path = base_path + "corpora/" + input_file
-# Path of the types and frequencies file
-typefreq_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + "_" + sim_tag + "_typefreq.txt"
-# Path of the similarity matrix
-similarities_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + \
-                         "_" + sim_tag + "_similarities.txt"
-# Results path file
-results_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_autocor.png"
+def compute_autocorrelation(working_path, input_file, sim_tag):
+    base_path = str.split(working_path, "SemSim_AutoCor")[0] + "SemSim_AutoCor/"
 
-# --- Load the data --- #
+    # Path of the text file
+    file_path = base_path + "corpora/" + input_file
+    # Path of the types and frequencies file
+    typefreq_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + "_" + sim_tag + "_typefreq.txt"
+    # Path of the similarity matrix
+    similarities_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + \
+                             "_" + sim_tag + "_similarities.txt"
+    # Results path file
+    results_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_autocor.png"
 
-# Import the type freq file
-type_freq_df = pd.read_csv(typefreq_file_path, sep=";", header=None)
-type_list = list(type_freq_df[0])
-freq_vec = np.array(type_freq_df[1])
-freq_vec = freq_vec / sum(freq_vec)
-n_type = len(type_list)  # The number of types
+    # --- Load the data --- #
 
-# Import the text file and remove non-existing token
-with open(file_path, "r") as text_file:
-    text_string = text_file.read()
-raw_token_list = nltk.word_tokenize(text_string)
-token_list = [token for token in raw_token_list if token in type_list]
-n_token = len(token_list)  # The number of tokens
+    # Import the type freq file
+    type_freq_df = pd.read_csv(typefreq_file_path, sep=";", header=None)
+    type_list = list(type_freq_df[0])
+    freq_vec = np.array(type_freq_df[1])
+    freq_vec = freq_vec / sum(freq_vec)
+    n_type = len(type_list)  # The number of types
 
-# Import the similarity matrix
-sim_mat = np.loadtxt(similarities_file_path, delimiter=";")
+    # Import the text file and remove non-existing token
+    with open(file_path, "r") as text_file:
+        text_string = text_file.read()
+    raw_token_list = nltk.word_tokenize(text_string)
+    token_list = [token for token in raw_token_list if token in type_list]
+    n_token = len(token_list)  # The number of tokens
 
-# --- Compute the dissimilarity matrix and the exchange matrix function ---#
+    # Import the similarity matrix
+    sim_mat = np.loadtxt(similarities_file_path, delimiter=";")
 
-# Easy computation of dissimilarity matrix (BUT CERTAINLY BETTER CHOICES ?)
-d_mat = 1 - sim_mat
+    # --- Compute the dissimilarity matrix and the exchange matrix function ---#
 
-# Function for the exchange matrix
-def exch_mat(r):
-    exch_m = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= r
-    np.fill_diagonal(exch_m, 0)
-    exch_m = exch_m / np.sum(exch_m)
-    return exch_m
+    # Easy computation of dissimilarity matrix (BUT CERTAINLY BETTER CHOICES ?)
+    d_mat = 1 - sim_mat
+
+    # Function for the exchange matrix
+    def exch_mat(r):
+        exch_m = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= r
+        np.fill_diagonal(exch_m, 0)
+        exch_m = exch_m / np.sum(exch_m)
+        return exch_m
 
 
-# --- Computation of the presence matrix ---#
+    # --- Computation of the presence matrix ---#
 
-# Compute the n_type x n_token presence matrix
-pres_mat = np.empty([0, n_token])
-for type_i in type_list:
-    pres_mat = np.append(pres_mat, [[token == type_i for token in token_list]], axis=0)
+    # Compute the n_type x n_token presence matrix
+    pres_mat = np.empty([0, n_token])
+    for type_i in type_list:
+        pres_mat = np.append(pres_mat, [[token == type_i for token in token_list]], axis=0)
 
-# --- Computation of the autocorrelation --- #
+    # --- Computation of the autocorrelation --- #
 
-# Computation of the global inertia
-global_inertia = np.sum(np.outer(freq_vec, freq_vec) * d_mat) / 2
+    # Computation of the global inertia
+    global_inertia = np.sum(np.outer(freq_vec, freq_vec) * d_mat) / 2
 
-# r_range
-r_range = list(range(1, max_range + 1))
+    # r_range
+    r_range = list(range(1, max_range + 1))
 
-# autocor_vec
-autocor_vec = []
-for r in tqdm(r_range):
-    # epsilon matrix (page 5 article)
-    epsilon_mat = pres_mat.dot(exch_mat(r).dot(pres_mat.T))
-    # local inertial
-    local_inertia = np.sum(epsilon_mat * d_mat) / 2
-    # autocorrelation index
-    autocor_index = (global_inertia - local_inertia) / global_inertia
-    # append
-    autocor_vec.append(autocor_index)
+    # autocor_vec
+    autocor_vec = []
+    for r in tqdm(r_range):
+        # epsilon matrix (page 5 article)
+        epsilon_mat = pres_mat.dot(exch_mat(r).dot(pres_mat.T))
+        # local inertial
+        local_inertia = np.sum(epsilon_mat * d_mat) / 2
+        # autocorrelation index
+        autocor_index = (global_inertia - local_inertia) / global_inertia
+        # append
+        autocor_vec.append(autocor_index)
 
-# --- Plot --- #
+    # --- Plot --- #
 
-plt.figure("autocorrelation")
-plt.scatter(r_range, autocor_vec)
-plt.plot(r_range, autocor_vec)
-plt.title(input_file + " | Sim: " + sim_tag + " | n tokens: " + str(n_token))
-plt.xlabel("Neighbourhood size r")
-plt.ylabel("Autocorrelation index")
-plt.savefig(results_file_path)
+    plt.figure("autocorrelation")
+    plt.scatter(r_range, autocor_vec)
+    plt.plot(r_range, autocor_vec)
+    plt.title(input_file + " | Sim: " + sim_tag + " | n tokens: " + str(n_token))
+    plt.xlabel("Neighbourhood size r")
+    plt.ylabel("Autocorrelation index")
+    plt.savefig(results_file_path)
+    plt.close()
+
+for input_file in input_files:
+    for sim_tag in sim_tags:
+        compute_autocorrelation(working_path, input_file, sim_tag)
--- code_python/4_compute_lisa.py
+++ code_python/4_compute_lisa.py
@@ -8,10 +8,9 @@ from scipy.linalg import expm
 # --- Parameters --- #
 
 # Path of the text file with only nouns, verbs, adjectives or adverbs to compute autocorrelation
-input_file = "Animal_farm_nouns.txt"
+input_files = ("The_WW_of_Oz_nouns.txt", "The_WW_of_Oz_verbs.txt", "Animal_farm_nouns.txt", "Animal_farm_verbs.txt")
+sim_tags = ("resnik", "wu-palmer", "leacock-chodorow") #, "wesim")
 
-# Name of the tag for the similarity
-sim_tag = "wesim"
 
 # Exchange matrix option ("u" = uniform, "d" = diffusive)
 exch_mat_opt = "d"
@@ -22,127 +21,134 @@ exch_range = 1000
 
 # Getting the base path (must run the script from a folder inside the "SemSim_Autocor" folder)
 working_path = os.getcwd()
-base_path = str.split(working_path, "SemSim_AutoCor")[0] + "SemSim_AutoCor/"
 
-# Path of the text file
-file_path = base_path + "corpora/" + input_file
-# Path of the types and frequencies file
-typefreq_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + "_" + sim_tag + "_typefreq.txt"
-# Path of the similarity matrix
-similarities_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + \
-                         "_" + sim_tag + "_similarities.txt"
-# Results path file
-results_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_lisa" + str(exch_range) + ".png"
+def compute_lisa(working_path, input_file, sim_tag, exch_mat_opt,exch_range ):
+    base_path = str.split(working_path, "SemSim_AutoCor")[0] + "SemSim_AutoCor/"
 
-reshtml_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_lisa" + str(exch_range) + ".html"
+    # Path of the text file
+    file_path = base_path + "corpora/" + input_file
+    # Path of the types and frequencies file
+    typefreq_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + "_" + sim_tag + "_typefreq.txt"
+    # Path of the similarity matrix
+    similarities_file_path = base_path + "similarities_frequencies/" + input_file[:-4] + \
+                             "_" + sim_tag + "_similarities.txt"
+    # Results path file
+    results_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_lisa" + str(exch_range) + ".png"
 
-# --- Load the data --- #
+    reshtml_file_path = base_path + "results/" + input_file[:-4] + "_" + sim_tag + "_lisa" + str(exch_range) + ".html"
 
-# Import the type freq file
-type_freq_df = pd.read_csv(typefreq_file_path, sep=";", header=None)
-type_list = list(type_freq_df[0])
-freq_vec = np.array(type_freq_df[1])
-freq_vec = freq_vec / sum(freq_vec)
-n_type = len(type_list)  # The number of types
+    # --- Load the data --- #
 
-# Import the text file and remove non-existing token
-with open(file_path, "r") as text_file:
-    text_string = text_file.read()
-raw_token_list = nltk.word_tokenize(text_string)
-token_list = [token for token in raw_token_list if token in type_list]
-n_token = len(token_list)  # The number of tokens
+    # Import the type freq file
+    type_freq_df = pd.read_csv(typefreq_file_path, sep=";", header=None)
+    type_list = list(type_freq_df[0])
+    freq_vec = np.array(type_freq_df[1])
+    freq_vec = freq_vec / sum(freq_vec)
+    n_type = len(type_list)  # The number of types
 
-# Import the similarity matrix
-sim_mat = np.loadtxt(similarities_file_path, delimiter=";")
+    # Import the text file and remove non-existing token
+    with open(file_path, "r") as text_file:
+        text_string = text_file.read()
+    raw_token_list = nltk.word_tokenize(text_string)
+    token_list = [token for token in raw_token_list if token in type_list]
+    n_token = len(token_list)  # The number of tokens
 
-# --- Computation --- #
+    # Import the similarity matrix
+    sim_mat = np.loadtxt(similarities_file_path, delimiter=";")
 
-# Compute the exchange matrix and the markov chain transition matrix
-f_vec = np.ones(n_token) / n_token
+    # --- Computation --- #
 
-if exch_mat_opt not in ["u", "d"]:
-    print("EXCHANGE MATRIX OPTION NOT RECOGNIZED, SETTING IT TO UNIFORM")
-    exch_mat_opt = "u"
+    # Compute the exchange matrix and the markov chain transition matrix
+    f_vec = np.ones(n_token) / n_token
 
-if exch_mat_opt == "u":
-    adj_mat = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= exch_range
-    np.fill_diagonal(adj_mat, 0)
-    g_vec = np.sum(adj_mat, axis=1) / np.sum(adj_mat)
-    k_vec = f_vec / g_vec
-    b_mat = np.array([[min(v1, v2) for v2 in g_vec] for v1 in g_vec]) * adj_mat / np.sum(adj_mat)
-    exch_mat = np.diag(f_vec) - np.diag(np.sum(b_mat, axis=1)) + b_mat
-else:
-    adj_mat = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= 1
-    np.fill_diagonal(adj_mat, 0)
-    l_adj_mat = np.diag(np.sum(adj_mat, axis=1)) - adj_mat
-    pi_outer_mat = np.outer(np.sqrt(f_vec), np.sqrt(f_vec))
-    phi_mat = (l_adj_mat / pi_outer_mat) / np.trace(l_adj_mat)
-    exch_mat = expm(- exch_range * phi_mat) * pi_outer_mat
+    if exch_mat_opt not in ["u", "d"]:
+        print("EXCHANGE MATRIX OPTION NOT RECOGNIZED, SETTING IT TO UNIFORM")
+        exch_mat_opt = "u"
 
-# Compute the exchange matrix and the markov chain transition matrix (OLD)
-# exch_m = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= lisa_range
-# np.fill_diagonal(exch_m, 0)
-# exch_m = exch_m / np.sum(exch_m)
-# f_vec = np.sum(exch_m, axis=0)
+    if exch_mat_opt == "u":
+        adj_mat = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= exch_range
+        np.fill_diagonal(adj_mat, 0)
+        g_vec = np.sum(adj_mat, axis=1) / np.sum(adj_mat)
+        k_vec = f_vec / g_vec
+        b_mat = np.array([[min(v1, v2) for v2 in g_vec] for v1 in g_vec]) * adj_mat / np.sum(adj_mat)
+        exch_mat = np.diag(f_vec) - np.diag(np.sum(b_mat, axis=1)) + b_mat
+    else:
+        adj_mat = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= 1
+        np.fill_diagonal(adj_mat, 0)
+        l_adj_mat = np.diag(np.sum(adj_mat, axis=1)) - adj_mat
+        pi_outer_mat = np.outer(np.sqrt(f_vec), np.sqrt(f_vec))
+        phi_mat = (l_adj_mat / pi_outer_mat) / np.trace(l_adj_mat)
+        exch_mat = expm(- exch_range * phi_mat) * pi_outer_mat
 
-w_mat = (exch_mat / np.sum(exch_mat, axis=1)).T
+    # Compute the exchange matrix and the markov chain transition matrix (OLD)
+    # exch_m = np.abs(np.add.outer(np.arange(n_token), -np.arange(n_token))) <= lisa_range
+    # np.fill_diagonal(exch_m, 0)
+    # exch_m = exch_m / np.sum(exch_m)
+    # f_vec = np.sum(exch_m, axis=0)
 
-# Easy computation of dissimilarity matrix
-d_mat = 1 - sim_mat
+    w_mat = (exch_mat / np.sum(exch_mat, axis=1)).T
 
-# Compute the n_type x n_token presence matrix
-pres_mat = np.empty([0, n_token])
-for type_i in type_list:
-    pres_mat = np.append(pres_mat, [[token == type_i for token in token_list]], axis=0)
+    # Easy computation of dissimilarity matrix
+    d_mat = 1 - sim_mat
 
-# Compute the extended distance matrix
-d_ext_mat = pres_mat.T.dot(d_mat.dot(pres_mat))
+    # Compute the n_type x n_token presence matrix
+    pres_mat = np.empty([0, n_token])
+    for type_i in type_list:
+        pres_mat = np.append(pres_mat, [[token == type_i for token in token_list]], axis=0)
 
-# Compute the centration matrix
-h_mat = np.identity(n_token) - np.outer(np.ones(n_token), f_vec)
+    # Compute the extended distance matrix
+    d_ext_mat = pres_mat.T.dot(d_mat.dot(pres_mat))
 
-# Compute the scalar produced matrix
-b_mat = - 0.5 * h_mat.dot(d_ext_mat.dot(h_mat.T))
+    # Compute the centration matrix
+    h_mat = np.identity(n_token) - np.outer(np.ones(n_token), f_vec)
 
-# Compute of the global inertia
-global_inertia = np.sum(np.outer(freq_vec, freq_vec) * d_mat) / 2
+    # Compute the scalar produced matrix
+    b_mat = - 0.5 * h_mat.dot(d_ext_mat.dot(h_mat.T))
 
-# Compute lisa
-lisa_vec = np.diag(w_mat.dot(b_mat)) / global_inertia
+    # Compute of the global inertia
+    global_inertia = np.sum(np.outer(freq_vec, freq_vec) * d_mat) / 2
 
-# --- Plot --- #
+    # Compute lisa
+    lisa_vec = np.diag(w_mat.dot(b_mat)) / global_inertia
 
-plt.figure("Lisa index")
-plt.plot(list(range(1, n_token + 1)), lisa_vec, linewidth=0.1)
-plt.title("{} | Sim: {} | Ntoken: {} | Exch: {} | Exch_range: {}".format(input_file, sim_tag, n_token, exch_mat_opt,
-                                                                         exch_range))
-plt.xlabel("text token")
-plt.ylabel("Lisa index")
-plt.savefig(results_file_path)
+    # --- Plot --- #
 
-# --- HTML output --- #
+    plt.figure("Lisa index")
+    plt.plot(list(range(1, n_token + 1)), lisa_vec, linewidth=0.1)
+    plt.title("{} | Sim: {} | Ntoken: {} | Exch: {} | Exch_range: {}".format(input_file, sim_tag, n_token, exch_mat_opt,
+                                                                             exch_range))
+    plt.xlabel("text token")
+    plt.ylabel("Lisa index")
+    plt.savefig(results_file_path)
+    plt.close()
 
-# vector of color Lisa
-color_lisa = np.copy(lisa_vec)
-color_lisa[color_lisa > 0] = (color_lisa[color_lisa > 0] - np.min(color_lisa[color_lisa > 0])) \
-                             / (np.max(lisa_vec) - np.min(color_lisa[color_lisa > 0])) * 255
-color_lisa[color_lisa < 0] = (color_lisa[color_lisa < 0] - np.max(color_lisa[color_lisa < 0])) \
-                             / np.abs(np.min(lisa_vec) - np.max(color_lisa[color_lisa < 0])) * 255
-color_lisa = np.intc(color_lisa)
+    # --- HTML output --- #
 
-with open(reshtml_file_path, 'w') as html_file:
-    html_file.write("<html>\n<head></head>\n")
-    html_file.write("<body><p>Input file: {} | Similarity tag: {} | Exchange matrix option: {} | "
-                    "Exchange matrix range: {}</p> <p>".format(input_file,
-                                                               sim_tag,
-                                                               exch_mat_opt,
-                                                               exch_range))
-    for i in range(len(token_list)):
-        if color_lisa[i] >= 0:
-            html_file.write("<span style=\"background-color: rgb({},255,{})\">".format(
-                255 - color_lisa[i], 255 - color_lisa[i]))
-        else:
-            html_file.write("<span style=\"background-color: rgb(255,{},{})\">".format(
-                255 + color_lisa[i], 255 + color_lisa[i]))
-        html_file.write(token_list[i] + " </span>")
-    html_file.write("</p></body>\n</html>")
+    # vector of color Lisa
+    color_lisa = np.copy(lisa_vec)
+    color_lisa[color_lisa > 0] = (color_lisa[color_lisa > 0] - np.min(color_lisa[color_lisa > 0])) \
+                                 / (np.max(lisa_vec) - np.min(color_lisa[color_lisa > 0])) * 255
+    color_lisa[color_lisa < 0] = (color_lisa[color_lisa < 0] - np.max(color_lisa[color_lisa < 0])) \
+                                 / np.abs(np.min(lisa_vec) - np.max(color_lisa[color_lisa < 0])) * 255
+    color_lisa = np.intc(color_lisa)
+
+    with open(reshtml_file_path, 'w') as html_file:
+        html_file.write("<html>\n<head></head>\n")
+        html_file.write("<body><p>Input file: {} | Similarity tag: {} | Exchange matrix option: {} | "
+                        "Exchange matrix range: {}</p> <p>".format(input_file,
+                                                                   sim_tag,
+                                                                   exch_mat_opt,
+                                                                   exch_range))
+        for i in range(len(token_list)):
+            if color_lisa[i] >= 0:
+                html_file.write("<span style=\"background-color: rgb({},255,{})\">".format(
+                    255 - color_lisa[i], 255 - color_lisa[i]))
+            else:
+                html_file.write("<span style=\"background-color: rgb(255,{},{})\">".format(
+                    255 + color_lisa[i], 255 + color_lisa[i]))
+            html_file.write(token_list[i] + " </span>")
+        html_file.write("</p></body>\n</html>")
+
+for input_file in input_files:
+    for sim_tag in sim_tags:
+        compute_lisa(working_path, input_file, sim_tag, exch_mat_opt,exch_range)
