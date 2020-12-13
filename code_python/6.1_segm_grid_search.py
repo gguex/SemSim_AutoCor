6da4ca8ad9adec5d6b8b1a250547af7493990cde
gguex <guillaume.guex@gmail.com>
2020-12-13 12:16:32 +0100

    1 more cut semisuper result, added loop to chain grid searches

--- code_python/6.1_segm_grid_search.py
+++ code_python/6.1_segm_grid_search.py
@@ -6,122 +6,140 @@ import random as rdm
 from sklearn.metrics import normalized_mutual_info_score
 
 # -------------------------------------
-# --- Parameters
+# --- Over-Parameters
 # -------------------------------------
 
-# File name to explore
-input_file = "mix_sent1_min5.txt"
-# Similarity tag
-sim_tag = "wesim"
-# File name to save
-results_file_name = "results_sent1_cut_1.csv"
-# Number of groups
-n_groups = 4
-# Ratio of known labels. If 0, clustering
-known_label_ratio = 0
-# Dist options to explore
-dist_option_vec = ["minus_log"]
-# Exchange matrix options to explore
-exch_mat_opt_vec = ["d"]
-exch_range_vec = [3, 5, 10, 15]
-# Parameter values to explore
-alpha_vec = [0.1, 1, 2, 5, 10, 50, 100]
-beta_vec = [0.1, 1, 5, 10, 50, 100, 300]
-kappa_vec = [0, 1 / 3, 2 / 3, 1]
-# Function to use for the segmentation
-# segm_function = discontinuity_segmentation
-segm_function = cut_segmentation
+input_file_list = ["mix_word1.txt",
+                   "mix_sent10_min5.txt",
+                   "mix_sent1_min5.txt",
+                   "mix_word3.txt",
+                   "mix_word1.txt"]
+results_file_name_list = ["results_semisuper10_word1_cut_1.csv",
+                          "results_semisuper5_sent10_cut_1.csv",
+                          "results_semisuper5_sent1_cut_1.csv"
+                          "results_semisuper5_word3_cut_1.csv"
+                          "results_semisuper5_word1_cut_1.csv"]
+known_label_ratio_list = [0.1, 0.05, 0.05, 0.05, 0.05]
 
-# -------------------------------------
-# --- Loading and preprocessing
-# -------------------------------------
+for i in range(len(input_file_list)):
 
-# Get the file paths
-text_file_path, typefreq_file_path, sim_file_path, ground_truth_path = get_all_paths(input_file, sim_tag)
+    # -------------------------------------
+    # --- Parameters
+    # -------------------------------------
 
-# Loading the similarity matrix
-sim_mat = np.loadtxt(sim_file_path, delimiter=";")
-# And the corresponding list of types
-with open(typefreq_file_path, 'r') as typefreq_file:
-    csv_reader = csv.reader(typefreq_file, delimiter=";")
-    type_list = [row[0] for row in csv_reader]
-# Compute the extended version of the similarity matrix
-sim_ext_mat, token_list = type_to_token_matrix_expansion(text_file_path, sim_mat, type_list)
+    # File name to explore
+    input_file = input_file_list[i]
+    # Similarity tag
+    sim_tag = "wesim"
+    # File name to save
+    results_file_name = results_file_name_list[i]
+    # Number of groups
+    n_groups = 4
+    # Ratio of known labels. If 0, clustering
+    known_label_ratio = known_label_ratio_list[i]
+    # Dist options to explore
+    dist_option_vec = ["minus_log"]
+    # Exchange matrix options to explore
+    exch_mat_opt_vec = ["d"]
+    exch_range_vec = [3, 5, 10, 15]
+    # Parameter values to explore
+    alpha_vec = [0.1, 1, 2, 5, 10, 50, 100]
+    beta_vec = [0.1, 1, 5, 10, 50, 100, 300]
+    kappa_vec = [0, 1 / 3, 2 / 3, 1]
+    # Function to use for the segmentation
+    # segm_function = discontinuity_segmentation
+    segm_function = cut_segmentation
 
-# Loading ground truth
-with open(ground_truth_path) as ground_truth:
-    real_group_vec = ground_truth.read()
-    real_group_vec = np.array([int(element) for element in real_group_vec.split(",")])
+    # -------------------------------------
+    # --- Loading and preprocessing
+    # -------------------------------------
 
-# For semi-supervised results, pick some labels
-if known_label_ratio > 0:
-    indices_for_known_label = rdm.sample(range(len(real_group_vec)), int(len(real_group_vec) * known_label_ratio))
-    known_labels = np.zeros(len(real_group_vec))
-    known_labels[indices_for_known_label] = real_group_vec[indices_for_known_label]
-    known_labels = known_labels.astype(int)
-else:
-    known_labels = []
-    indices_for_known_label = []
+    # Get the file paths
+    text_file_path, typefreq_file_path, sim_file_path, ground_truth_path = get_all_paths(input_file, sim_tag)
 
-# Make results file
-with open(results_file_name, "w") as output_file:
-    output_file.write("dist,exch_opt,exch_range,alpha,beta,kappa,nmi\n")
+    # Loading the similarity matrix
+    sim_mat = np.loadtxt(sim_file_path, delimiter=";")
+    # And the corresponding list of types
+    with open(typefreq_file_path, 'r') as typefreq_file:
+        csv_reader = csv.reader(typefreq_file, delimiter=";")
+        type_list = [row[0] for row in csv_reader]
+    # Compute the extended version of the similarity matrix
+    sim_ext_mat, token_list = type_to_token_matrix_expansion(text_file_path, sim_mat, type_list)
 
-# -------------------------------------
-# --- Grid search
-# -------------------------------------
+    # Loading ground truth
+    with open(ground_truth_path) as ground_truth:
+        real_group_vec = ground_truth.read()
+        real_group_vec = np.array([int(element) for element in real_group_vec.split(",")])
 
-print("Starting Grid search")
+    # For semi-supervised results, pick some labels
+    if known_label_ratio > 0:
+        indices_for_known_label = rdm.sample(range(len(real_group_vec)), int(len(real_group_vec) * known_label_ratio))
+        known_labels = np.zeros(len(real_group_vec))
+        known_labels[indices_for_known_label] = real_group_vec[indices_for_known_label]
+        known_labels = known_labels.astype(int)
+    else:
+        known_labels = []
+        indices_for_known_label = []
 
-for dist_option in dist_option_vec:
+    # Make results file
+    with open(results_file_name, "w") as output_file:
+        output_file.write("dist,exch_opt,exch_range,alpha,beta,kappa,nmi\n")
 
-    # Compute the dissimilarity matrix
-    d_ext_mat = similarity_to_dissimilarity(sim_ext_mat,
-                                            dist_option=dist_option)
+    # -------------------------------------
+    # --- Grid search
+    # -------------------------------------
 
-    print(f"Dissimilarity matrix computed with {dist_option}")
+    print("Starting Grid search")
 
-    for exch_mat_opt in exch_mat_opt_vec:
-        for exch_range in exch_range_vec:
+    for dist_option in dist_option_vec:
 
-            # Compute the exchange and transition matrices
-            exch_mat, w_mat = exchange_and_transition_matrices(len(token_list),
-                                                               exch_mat_opt=exch_mat_opt,
-                                                               exch_range=exch_range)
+        # Compute the dissimilarity matrix
+        d_ext_mat = similarity_to_dissimilarity(sim_ext_mat,
+                                                dist_option=dist_option)
 
-            print(f"Exchange matrix computed with {exch_mat_opt} and range {exch_range}")
+        print(f"Dissimilarity matrix computed with {dist_option}")
 
-            for alpha in alpha_vec:
-                for beta in beta_vec:
-                    for kappa in kappa_vec:
-                        # Compute the matrix
-                        if known_label_ratio > 0:
-                            result_matrix = segm_function(d_ext_mat=d_ext_mat,
-                                                          exch_mat=exch_mat,
-                                                          w_mat=w_mat,
-                                                          n_groups=4,
-                                                          alpha=alpha,
-                                                          beta=beta,
-                                                          kappa=kappa,
-                                                          init_labels=known_labels)
-                        else:
-                            result_matrix = segm_function(d_ext_mat=d_ext_mat,
-                                                          exch_mat=exch_mat,
-                                                          w_mat=w_mat,
-                                                          n_groups=4,
-                                                          alpha=alpha,
-                                                          beta=beta,
-                                                          kappa=kappa)
+        for exch_mat_opt in exch_mat_opt_vec:
+            for exch_range in exch_range_vec:
 
-                        # Compute the groups
-                        algo_group_value = np.argmax(result_matrix, 1) + 1
+                # Compute the exchange and transition matrices
+                exch_mat, w_mat = exchange_and_transition_matrices(len(token_list),
+                                                                   exch_mat_opt=exch_mat_opt,
+                                                                   exch_range=exch_range)
 
-                        # Compute nmi score
-                        nmi = normalized_mutual_info_score(np.delete(real_group_vec, indices_for_known_label),
-                                                           np.delete(algo_group_value, indices_for_known_label))
-                        print(f"NMI = {nmi}")
+                print(f"Exchange matrix computed with {exch_mat_opt} and range {exch_range}")
 
-                        # Writing results
-                        with open(results_file_name, "a") as output_file:
-                            output_file.write(f"{dist_option},{exch_mat_opt},{exch_range},{alpha},{beta},{kappa},"
-                                              f"{nmi}\n")
+                for alpha in alpha_vec:
+                    for beta in beta_vec:
+                        for kappa in kappa_vec:
+                            # Compute the matrix
+                            if known_label_ratio > 0:
+                                result_matrix = segm_function(d_ext_mat=d_ext_mat,
+                                                              exch_mat=exch_mat,
+                                                              w_mat=w_mat,
+                                                              n_groups=4,
+                                                              alpha=alpha,
+                                                              beta=beta,
+                                                              kappa=kappa,
+                                                              init_labels=known_labels)
+                            else:
+                                result_matrix = segm_function(d_ext_mat=d_ext_mat,
+                                                              exch_mat=exch_mat,
+                                                              w_mat=w_mat,
+                                                              n_groups=4,
+                                                              alpha=alpha,
+                                                              beta=beta,
+                                                              kappa=kappa)
+
+                            # Compute the groups
+                            algo_group_value = np.argmax(result_matrix, 1) + 1
+
+                            # Compute nmi score
+                            nmi = normalized_mutual_info_score(np.delete(real_group_vec, indices_for_known_label),
+                                                               np.delete(algo_group_value, indices_for_known_label))
+                            print(f"NMI = {nmi}")
+
+                            # Writing results
+                            with open(results_file_name, "a") as output_file:
+                                output_file.write(f"{dist_option},{exch_mat_opt},{exch_range},{alpha},{beta},{kappa},"
+                                                  f"{nmi}\n")
