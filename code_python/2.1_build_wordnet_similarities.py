430f465863ca2d62a9d5113fc66219253e2486ea
gguex <guillaume.guex@gmail.com>
2021-01-07 12:01:53 +0100

    Prepared 2.1 and 2.2 for serial computations

--- code_python/2.1_build_wordnet_similarities.py
+++ code_python/2.1_build_wordnet_similarities.py
@@ -9,97 +9,103 @@ import itertools
 # --- Parameters
 # -------------------------------------
 
-# Path of the text file with only nouns, verbs, adjectives or adverbs
-input_file = "The_WW_of_Oz_pp.txt"
+# List of paths for text files to compute similarity
+input_file_list = ["Lectures_on_Landscape_pp.txt",
+                   "Metamorphosis_pp.txt",
+                   "Civil_Disobedience_pp.txt",
+                   "Sidelights_on_relativity_pp.txt"]
 
-# Name of the outputted tag for the similarity ("wup", "path", "resb" or "jcnb")
-sim_tag = "jcnb"
+# List of tags to enumerate similarity to compute
+sim_tag_list = ["wup", "path", "resb", "jcnb"]
 
 # -------------------------------------
 # --- Computations
 # -------------------------------------
 
-# --- Defining similarity --- #
-
+# Loading brown corpus information
 brown_ic = wordnet_ic.ic('ic-brown.dat')
 
-if sim_tag == "wup":
-    def wn_similarity(synset_1, synset_2):
-        return wn.wup_similarity(synset_1, synset_2)
-elif sim_tag == "path":
-    def wn_similarity(synset_1, synset_2):
-        return wn.path_similarity(synset_1, synset_2)
-elif sim_tag == "resb":
-    def wn_similarity(synset_1, synset_2):
-        if synset_1.pos() not in ["a", "s", "r"] and synset_2.pos() not in ["a", "s", "r"]:
-            return wn.res_similarity(synset_1, synset_2, brown_ic)
-        else:
-            return None
-else:
-    def wn_similarity(synset_1, synset_2):
-        if synset_1.pos() not in ["a", "s", "r"] and synset_2.pos() not in ["a", "s", "r"]:
-            return wn.jcn_similarity(synset_1, synset_2, brown_ic)
-        else:
-            return None
+# Loop on files and tags
+for input_file in input_file_list:
+    for sim_tag in sim_tag_list:
 
-# --- Defining paths --- #
+        # Print which is computed
+        print(f"Computing similarity {sim_tag} for corpus {input_file}")
 
-# Getting all paths
-file_path, type_freq_file_path, sim_matrix_file_path, _ = get_all_paths(input_file, sim_tag, warn=False)
-
-# --- Get token, type and freq --- #
-
-# Opening the file
-with open(file_path, "r") as text_file:
-    text_string = text_file.read()
-
-# Split by tokens
-token_list = nltk.word_tokenize(text_string)
-
-# Get type list and frequencies
-type_freq_dict = nltk.FreqDist(token_list)
-vocab_text = set(type_freq_dict.keys())
-
-# build vocabulary
-vocab_in_wordnet = [type for type in vocab_text if len(wn.synsets(type)) > 0]
-
-# Build autosim to check if any synset is connected
-auto_sim_list = []
-checked_vocab_in_wordnet = []
-for type in vocab_in_wordnet:
-    type_synsets_list = wn.synsets(type)
-    sim_list = [wn_similarity(type_synsets, type_synsets) for type_synsets in type_synsets_list
-                if wn_similarity(type_synsets, type_synsets) is not None]
-    if len(sim_list) > 0:
-        auto_sim_list.append(max(sim_list))
-        checked_vocab_in_wordnet.append(type)
-
-# Write the two files
-with open(type_freq_file_path, "w") as type_freq_file, open(sim_matrix_file_path, "w") as sim_matrix_file:
-    for type_1 in tqdm(checked_vocab_in_wordnet):
-
-        type_1_synsets_list = wn.synsets(type_1)
-        type_freq_file.write(type_1 + ";" + str(type_freq_dict[type_1]) + "\n")
-
-        for i, type_2 in enumerate(checked_vocab_in_wordnet):
-
-            if type_2 != type_1:
-                # Loop on synsets
-                type_2_synsets_list = wn.synsets(type_2)
-                sim_list = [wn_similarity(*cross_item)
-                            for cross_item in itertools.product(type_1_synsets_list, type_2_synsets_list)
-                            if cross_item[0].pos() == cross_item[1].pos() and
-                            wn_similarity(*cross_item) is not None]
-                if len(sim_list) > 0:
-                    sim = max(sim_list)
+        # Defining similarity
+        if sim_tag == "wup":
+            def wn_similarity(synset_1, synset_2):
+                return wn.wup_similarity(synset_1, synset_2)
+        elif sim_tag == "path":
+            def wn_similarity(synset_1, synset_2):
+                return wn.path_similarity(synset_1, synset_2)
+        elif sim_tag == "resb":
+            def wn_similarity(synset_1, synset_2):
+                if synset_1.pos() not in ["a", "s", "r"] and synset_2.pos() not in ["a", "s", "r"]:
+                    return wn.res_similarity(synset_1, synset_2, brown_ic)
                 else:
-                    sim = 0
-            else:
-                sim = auto_sim_list[i]
+                    return None
+        else:
+            def wn_similarity(synset_1, synset_2):
+                if synset_1.pos() not in ["a", "s", "r"] and synset_2.pos() not in ["a", "s", "r"]:
+                    return wn.jcn_similarity(synset_1, synset_2, brown_ic)
+                else:
+                    return None
 
-            # Write the similarity
-            sim_matrix_file.write(str(sim))
-            if type_2 != checked_vocab_in_wordnet[len(checked_vocab_in_wordnet) - 1]:
-                sim_matrix_file.write(";")
-            else:
-                sim_matrix_file.write("\n")
+        # Getting all paths
+        file_path, type_freq_file_path, sim_matrix_file_path, _ = get_all_paths(input_file, sim_tag, warn=False)
+
+        # Opening the file
+        with open(file_path, "r") as text_file:
+            text_string = text_file.read()
+
+        # Split by tokens
+        token_list = nltk.word_tokenize(text_string)
+
+        # Get type list and frequencies
+        type_freq_dict = nltk.FreqDist(token_list)
+        vocab_text = set(type_freq_dict.keys())
+
+        # build vocabulary
+        vocab_in_wordnet = [type for type in vocab_text if len(wn.synsets(type)) > 0]
+
+        # Build autosim to check if any synset is connected
+        auto_sim_list = []
+        checked_vocab_in_wordnet = []
+        for type in vocab_in_wordnet:
+            type_synsets_list = wn.synsets(type)
+            sim_list = [wn_similarity(type_synsets, type_synsets) for type_synsets in type_synsets_list
+                        if wn_similarity(type_synsets, type_synsets) is not None]
+            if len(sim_list) > 0:
+                auto_sim_list.append(max(sim_list))
+                checked_vocab_in_wordnet.append(type)
+
+        # Write the two files
+        with open(type_freq_file_path, "w") as type_freq_file, open(sim_matrix_file_path, "w") as sim_matrix_file:
+            for type_1 in tqdm(checked_vocab_in_wordnet):
+
+                type_1_synsets_list = wn.synsets(type_1)
+                type_freq_file.write(type_1 + ";" + str(type_freq_dict[type_1]) + "\n")
+
+                for i, type_2 in enumerate(checked_vocab_in_wordnet):
+
+                    if type_2 != type_1:
+                        # Loop on synsets
+                        type_2_synsets_list = wn.synsets(type_2)
+                        sim_list = [wn_similarity(*cross_item)
+                                    for cross_item in itertools.product(type_1_synsets_list, type_2_synsets_list)
+                                    if cross_item[0].pos() == cross_item[1].pos() and
+                                    wn_similarity(*cross_item) is not None]
+                        if len(sim_list) > 0:
+                            sim = max(sim_list)
+                        else:
+                            sim = 0
+                    else:
+                        sim = auto_sim_list[i]
+
+                    # Write the similarity
+                    sim_matrix_file.write(str(sim))
+                    if type_2 != checked_vocab_in_wordnet[len(checked_vocab_in_wordnet) - 1]:
+                        sim_matrix_file.write(";")
+                    else:
+                        sim_matrix_file.write("\n")
--- code_python/2.2_build_wordvectors_similarities.py
+++ code_python/2.2_build_wordvectors_similarities.py
@@ -7,59 +7,60 @@ from code_python.local_functions import get_all_paths
 # --- Parameters
 # -------------------------------------
 
-# Path of the text file with only nouns, verbs, adjectives or adverbs
-input_file = "The_WW_of_Oz_pp.txt"
+# List of paths for text files to compute similarity
+input_file_list = ["Lectures_on_Landscape_pp.txt",
+                   "Metamorphosis_pp.txt",
+                   "Civil_Disobedience_pp.txt",
+                   "Sidelights_on_relativity_pp.txt"]
 
-# Name of the outputted tag for the similarity ("w2v" or "glv")
-sim_tag = "glv"
+# List of tags to enumerate similarity to compute
+sim_tag_list = ["w3v", "glv"]
 
 # -------------------------------------
 # --- Computations
 # -------------------------------------
 
-# Path of the Word vector model
-if sim_tag == "w2v":
-    wv_model_path = "/home/gguex/Documents/data/pretrained_word_vectors/enwiki.model"
-else:
-    wv_model_path = "/home/gguex/Documents/data/pretrained_word_vectors/glove42B300d.model"
+# Loading wordvector models
+w2v_model = KeyedVectors.load("/home/gguex/Documents/data/pretrained_word_vectors/enwiki.model")
+glv_model = KeyedVectors.load("/home/gguex/Documents/data/pretrained_word_vectors/glove42B300d.model")
 
+# Loop on files and tags
+for input_file in input_file_list:
+    for sim_tag in sim_tag_list:
 
-# --- Defining paths --- #
+        # Getting all paths
+        file_path, type_freq_file_path, sim_matrix_file_path, _ = get_all_paths(input_file, sim_tag, warn=False)
 
-# Getting all paths
-file_path, type_freq_file_path, sim_matrix_file_path, _ = get_all_paths(input_file, sim_tag, warn=False)
+        # Opening the file
+        with open(file_path, "r") as text_file:
+            text_string = text_file.read()
 
-# --- Get token, type and freq --- #
+        # Split by tokens
+        token_list = nltk.word_tokenize(text_string)
 
-# Opening the file
-with open(file_path, "r") as text_file:
-    text_string = text_file.read()
+        # Get type list and frequencies
+        type_freq_dict = nltk.FreqDist(token_list)
+        vocab_text = set(type_freq_dict.keys())
 
-# Split by tokens
-token_list = nltk.word_tokenize(text_string)
+        # select gensim model
+        if sim_tag == "w2v":
+            wv_model = w2v_model
+        else:
+            wv_model = glv_model
 
-# Get type list and frequencies
-type_freq_dict = nltk.FreqDist(token_list)
-vocab_text = set(type_freq_dict.keys())
+        # build vocabulary
+        vocab_wv = set(wv_model.vocab.keys())
 
-# --- Load the gensim model, check common vocabulary and write files ---#
+        # Find the common vocabulary
+        vocab_common = list(vocab_wv & vocab_text)
 
-# load gensim model
-wv_model = KeyedVectors.load(wv_model_path)
-
-# build vocabulary
-vocab_wv = set(wv_model.vocab.keys())
-
-# Find the common vocabulary
-vocab_common = list(vocab_wv & vocab_text)
-
-# Write the two files
-with open(type_freq_file_path, "w") as type_freq_file, open(sim_matrix_file_path, "w") as sim_matrix_file:
-    for type_1 in tqdm(vocab_common):
-        type_freq_file.write(type_1 + ";" + str(type_freq_dict[type_1]) + "\n")
-        for type_2 in vocab_common:
-            sim_matrix_file.write(str(wv_model.similarity(type_1, type_2)))
-            if type_2 != vocab_common[len(vocab_common) - 1]:
-                sim_matrix_file.write(";")
-            else:
-                sim_matrix_file.write("\n")
+        # Write the two files
+        with open(type_freq_file_path, "w") as type_freq_file, open(sim_matrix_file_path, "w") as sim_matrix_file:
+            for type_1 in tqdm(vocab_common):
+                type_freq_file.write(type_1 + ";" + str(type_freq_dict[type_1]) + "\n")
+                for type_2 in vocab_common:
+                    sim_matrix_file.write(str(wv_model.similarity(type_1, type_2)))
+                    if type_2 != vocab_common[len(vocab_common) - 1]:
+                        sim_matrix_file.write(";")
+                    else:
+                        sim_matrix_file.write("\n")
