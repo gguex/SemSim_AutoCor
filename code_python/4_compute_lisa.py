56ab800193377eb5d5b0257544bc1ddac7839ab9
gguex <guillaume.guex@gmail.com>
2021-01-13 09:52:19 +0100

    New lisa results computed

--- code_python/4_compute_lisa.py
+++ code_python/4_compute_lisa.py
@@ -16,7 +16,7 @@ input_file_list = ["Civil_Disobedience_pp.txt",
                    "Sidelights_on_relativity_pp.txt"]
 
 # Similarity tag list
-sim_tag_list = ["glv", "path", "resb", "w2v", "wup"]
+sim_tag_list = ["lch", "path", "wup"]
 
 # Distance option
 dist_option = "max_minus"
