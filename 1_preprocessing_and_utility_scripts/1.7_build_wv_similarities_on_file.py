fdb8c2b00c8f47f363a11cd0767d256e4f62fb31
gguex <guillaume.guex@gmail.com>
2021-11-17 16:36:36 +0100

    Created a single similarities construction script

---
+++ 1_preprocessing_and_utility_scripts/1.7_build_wv_similarities_on_file.py
@@ -0,0 +1,38 @@
+from gensim.models import KeyedVectors
+from local_functions import build_wv_similarity_matrix
+from os.path import expanduser
+
+# -------------------------------------
+# --- Parameters
+# -------------------------------------
+
+# Input folder
+input_file = "corpora/manifesto_pp/61320_201211_pp_wostw.txt"
+
+# List of tags to enumerate similarity to compute
+sim_tag_list = ["w2v", "glv", "ftx"]
+
+# -------------------------------------
+# --- Computation
+# -------------------------------------
+
+# Short name
+short_file_name = input_file.split("/")[-1:][0][:-4]
+
+# Loading wordvector models
+home = expanduser("~")
+w2v_model = KeyedVectors.load(f"{home}/Documents/data/pretrained_word_vectors/enwiki.model")
+glv_model = KeyedVectors.load(f"{home}/Documents/data/pretrained_word_vectors/glove42B300d.model")
+ftx_model = KeyedVectors.load(f"{home}/Documents/data/pretrained_word_vectors/en_fasttext.model")
+
+# Loop on sim tag
+for sim_tag in sim_tag_list:
+    if sim_tag == "w2v":
+        build_wv_similarity_matrix(input_file,
+                                   f"similarity_matrices/{short_file_name}_{sim_tag}.csv", w2v_model)
+    elif sim_tag == "glv":
+        build_wv_similarity_matrix(input_file,
+                                   f"similarity_matrices/{short_file_name}_{sim_tag}.csv", glv_model)
+    elif sim_tag == "ftx":
+        build_wv_similarity_matrix(input_file,
+                                   f"similarity_matrices/{short_file_name}_{sim_tag}.csv", ftx_model)
